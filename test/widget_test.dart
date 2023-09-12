2692aa7d84e8bd67d89c45d4abd11f1bf6d9f211
ahmad-frehan <ahmad-frehan@futurecode-co.com>
2023-09-12 21:15:19 +0300

    init

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -4,8 +4,7 @@
 // utility in the flutter_test package. For example, you can send tap and scroll
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:howtotesting/main.dart';
 
@@ -16,13 +15,11 @@ void main() {
     expect(x, 2);
   });
   testWidgets('find txst', (WidgetTester tester) async {
-    );
     await tester
         .pumpWidget(const MyWidget(title: 'ahmad', message: 'ahmadalfrehan'));
-
     expect(find.text('ahmad'), findsOneWidget);
-
     expect(find.text('ahmadalfrehan'), findsOneWidget);
+    expect(find.widgetWithText(Center, 'ahmadalfrehan'), findsOneWidget);
     await tester.pump();
   });
 }
